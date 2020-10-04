96f6bde6f41c11a46c8890c346c99867a3c56604
Radek Ziemniewicz <radoslaw@ziemniewicz.pl>
2020-10-04 14:13:51 +0100

    Moved stream class from Request repo

---
+++ src/QuillStack/Http/Stream/InputStream.php
@@ -0,0 +1,151 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QuillStack\Http\Request;
+
+use Psr\Http\Message\StreamInterface;
+
+final class InputStream implements StreamInterface
+{
+    /**
+     * @var string|null
+     */
+    private ?string $body;
+
+    /**
+     * @param null $contest
+     */
+    public function __construct($contest = null)
+    {
+        $body = $contest ?? file_get_contents('php://input');
+        $this->body = !empty($body) ? $body : null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function __toString()
+    {
+        return $this->body;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function close()
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function detach()
+    {
+        $body = $this->body;
+        $this->body = null;
+
+        return $body;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getSize()
+    {
+        return strlen($this->body);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function tell()
+    {
+        return 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function eof()
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isSeekable()
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function seek($offset, $whence = SEEK_SET)
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function rewind()
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isWritable()
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function write($string)
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function isReadable()
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function read($length)
+    {
+        return $this->body;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getContents()
+    {
+        return $this->body;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getMetadata($key = null)
+    {
+        if ($key === null) {
+            return [];
+        }
+
+        return null;
+    }
+}
